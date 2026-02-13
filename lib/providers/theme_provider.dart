1a755a1eeb38f0fc1a49502e84053763deb575bc
ArticProject <mihailulybin67@gmail.com>
2026-02-13 06:47:42 +0300

    Create theme_provider.dart

---
+++ lib/providers/theme_provider.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final themeModeProvider = StateNotifierProvider<ThemeNotifier, ThemeMode>((ref) {
+  return ThemeNotifier();
+});
+
+class ThemeNotifier extends StateNotifier<ThemeMode> {
+  ThemeNotifier() : super(ThemeMode.system) {
+    _loadTheme();
+  }
+
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final saved = prefs.getString('theme_mode');
+    if (saved != null) {
+      state = ThemeMode.values.firstWhere(
+        (e) => e.toString() == saved,
+        orElse: () => ThemeMode.system,
+      );
+    }
+  }
+
+  Future<void> setTheme(ThemeMode mode) async {
+    state = mode;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('theme_mode', mode.toString());
+  }
+}
